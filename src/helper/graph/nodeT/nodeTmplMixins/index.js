b8098ed91ebe0a5cc626fc2ea3513a50c01319b7
chesterlgs <chester_01@qq.com>
2019-06-23 15:41:31 +0800

    remove graph

--- src/helper/graph/nodeT/nodeTmplMixins/index.js
+++
@@ -1,8 +0,0 @@
-import genDoubtTips from './doubtTips'
-import makeLinkPort from './linkPort'
-import genLoading from './loading'
-import genPosition from './position'
-import genTagging from './tagging'
-
-export {genDoubtTips, makeLinkPort, genLoading, genPosition, genTagging};
-
